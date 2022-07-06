e8d84ec0ef472052c927291c1ef7552252be9c5f
sahoodcom <sahoodhkr@gmail.com>
2022-07-06 21:03:05 +0530

    routing solved

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ const App = () => {
     
     return (
         <div className={currentMode === "Dark" ? "dark" : ""} >
-            <BrowserRouter>
+    <BrowserRouter basename="/dashboard" >
+            {/* <BrowserRouter> */}
                 <div className='flex relative dark:bg-main-dark-bg' >
                     <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }} >
                         <TooltipComponent content='Settings' position="Top">
